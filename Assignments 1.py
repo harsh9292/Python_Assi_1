3973bc6fdb1d72de4e47b460967fbbb5632f229d
harsh9292 <sonkusaleharsh29@gmail.com>
2025-07-20 13:52:48 +0530

    Add files via upload

---
+++ Assignments 1.py
@@ -0,0 +1,40 @@
+# Assignment 1
+
+# Task 1: Perform Basic Mathematical Operations
+"""
+Addition
+Subtraction
+Multiplication
+Division
+"""
+
+"""
+a=float(input('Enter 1st no.:'))
+b=float(input('Enter 2nd no.:'))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+fdiv=a//b
+
+Addition=add
+Subtraction=sub
+Multiplication=mul
+Division=div
+Floor_Division=fdiv
+
+
+print('Addition of',a,'and',b,'is',add)
+print('Substraction of',a,'and',b,'is',sub)
+print('Multiplication of',a,'and',b,'is',mul)
+print('Division of',a,'and',b,'is',div)
+print('Floor Division of',a,'and',b,'is',fdiv)
+"""
+
+# Task 2: Create a Personalized Greeting
+
+f=(input('Enter your First Name:'))
+l=(input('Enter your Last Name:'))
+
+print('Hello',f,l,'! Welcome to the World Of Python')
